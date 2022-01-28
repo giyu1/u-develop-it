1d8933e6fcd18c29c19fb7a563e42e90959f9987
Giyu1 <sameer.m.waris@gmail.com>
2022-01-28 16:20:38 -0700

    set-up connection to candidates table in server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-// This variableis importing the express package 
+// This variable is importing the express package 
 const express = require('express');
-
+// Connecting the mysql2 database 
+const mysql = require('mysql2');
 // Setting up the PORT and calling the express package 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,12 +13,33 @@ const app = express();
 //     });
 // });
 
+
 // Express middleware 
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Waffles12345',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// This query method runs SQL query and executes a callback 
+// w/ resulting rows that match the query 
+db.query(`Select * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 // Default response for any other request (Not Found)
-app.use((req, res) => {
+// This is a catch-all route b/c the "*" is considerd a 'wild card'
+app.use("*", (req, res) => {
     res.status(404).end();
 });
 
