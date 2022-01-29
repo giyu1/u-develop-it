e01484af52d52ae27b458cf0af98d7714de9d064
Giyu1 <sameer.m.waris@gmail.com>
2022-01-28 17:41:11 -0700

    created db.query to get, delete and create candidates

--- server.js
+++ server.js
@@ -33,8 +33,36 @@ const db = mysql.createConnection(
 
 // This query method runs SQL query and executes a callback 
 // w/ resulting rows that match the query 
-db.query(`Select * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// db.query(`Select * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if(err) {
+//         console.log(err)
+//     }
+//     console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+             VALUES(?,?,?,?)`
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, results) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log(results);
 });
 
 // Default response for any other request (Not Found)
