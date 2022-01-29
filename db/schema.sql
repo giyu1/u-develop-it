df6872845a8be6efd41fb74347bb9561365c4b15
Giyu1 <sameer.m.waris@gmail.com>
2022-01-29 06:50:54 -0700

    created parties table and schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
+CREATE TABLE parties (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+); 
+
 -- This is created to show the Tables schema -- s
 CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY, 
@@ -5,3 +11,5 @@ CREATE TABLE candidates (
     last_name VARCHAR(30) NOT NULL, 
     industry_connected BOOLEAN NOT NULL
 );
+
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -10,3 +10,11 @@ VALUES
 ('Montague', 'Summers', 1),
 ('Octavia', 'Butler', 1),
 ('Unica', 'Zurn', 1);
+
+-- Similar to the candidates table, this information in parties --
+-- will be seeded into the parties schema located under the db -- 
+INSERT INTO parties (name, description)
+VALUES
+    ('JS Juggernauts', 'The JS Juggernaut eat, breather, and sleep JavaScript. They can build everything you could ever want in SJ, including a new kitchen sink.'),
+    ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'),
+    ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
